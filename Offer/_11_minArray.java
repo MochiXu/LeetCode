4393e451d70919ea8988052784f59d64d6d2ec75
MochiXu <mirage_mc@icloud.com>
2022-02-17 08:28:26 +0800

    offer11 最小数字 E

---
+++ Offer/_11_minArray.java
@@ -0,0 +1,16 @@
+package Offer;
+/**
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ *
+ * 给你一个可能存在重复元素值的数组numbers，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为1。
+ *
+ * */
+public class _11_minArray {
+    public int minArray(int[] numbers) {
+        for (int i=1;i<numbers.length;i++){
+            if (numbers[i-1]>numbers[i])
+                return numbers[i];
+        }
+        return numbers[0];
+    }
+}
